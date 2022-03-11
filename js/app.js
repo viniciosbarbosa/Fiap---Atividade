9c5eb441bf0c0e48cc5a9abba8ee05e865807b1e
vinicios.barbosa <vinicios.barbosa@itlean.com.br>
2022-03-10 22:35:44 -0300

    Atividade Fiap

---
+++ js/app.js
@@ -0,0 +1,28 @@
+(function(){
+    function slider(sliderName, velocidade) {
+        var sliderClass = '.' + sliderName,
+                activeClass = 'active',
+                rotate = setInterval(rotateSlide, velocidade);
+        
+        $(sliderClass + ' > :first').addClass(activeClass);
+    
+        $(sliderClass).hover(function(){
+            clearInterval(rotate);
+        }, function() {
+            rotate = setInterval(rotateSlide, velocidade);
+        });
+        
+        function rotateSlide() {
+            var activeSlide = $(sliderClass + ' > .' + activeClass),
+                    nextSlide = activeSlide.next();
+    
+            if(nextSlide.length == 0) {
+                nextSlide = $(sliderClass + ' > :first');
+            }
+            activeSlide.removeClass(activeClass);
+            nextSlide.addClass(activeClass);
+        } 
+    }
+    
+    slider('introducao', 2000);
+    })();
